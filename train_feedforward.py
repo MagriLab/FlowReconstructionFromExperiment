6f8f8da35ffbfbdefcdcf1f7c6a8d0e06671ac44
y.mo <97536008+yaxinm@users.noreply.github.com>
2022-11-10 19:29:27 +0000

    added training script for feedforward network

--- train_feedforward.py
+++ train_feedforward.py
@@ -17,26 +17,33 @@ import time
 
 train_test_split = [600,100,100]
 learning_rate = 0.0001
-epochs = 160000
-# epochs = 1
+epochs = 12000
+# epochs = 5
 
 print("Started at: ", time.asctime(time.localtime(time.time())))
 
 # ======================= pre-processing =========================
 (ux,uy,pp) = project.read_data(Path("./local_data/re100"),132)
+x = np.stack([ux,uy,pp],axis=0)
 
-[ux_train,ux_val,ux_test], [ux_train_m,ux_val_m,ux_test_m] = data_partition(ux,0,train_test_split,REMOVE_MEAN=True)
-[uy_train,uy_val,uy_test], [uy_train_m,uy_val_m,uy_test_m] = data_partition(uy,0,train_test_split,REMOVE_MEAN=True)
-pb = pp[:,0,49:80] # pressure at the base
-[pb_train,pb_val,pb_test], [pb_train_m,pb_val_m,pb_test_m] = data_partition(pb,0,train_test_split,REMOVE_MEAN=True)
+[x_train,x_val,x_test], [xm_train,xm_val,xm_test] = data_partition(x,1,train_test_split,REMOVE_MEAN=True)
 
-(nt,nx,ny) = ux.shape
+[ux_train,uy_train,pp_train] = np.squeeze(np.split(x_train,3,axis=0))
+[ux_val,uy_val,pp_val] = np.squeeze(np.split(x_val,3,axis=0))
+[ux_test,uy_test,pp_test] = np.squeeze(np.split(x_test,3,axis=0))
+
+pb_train = pp_train[:,0,49:80] # pressure at the base
+pb_val = pp_val[:,0,49:80] # pressure at the base
+pb_test = pp_test[:,0,49:80] # pressure at the base
+
+(nt,nx,ny) = ux_train.shape
+n_base = pb_train.shape[-1]
 
 # ==================== define network ============================
 
 # set up model
 def feedforward(x):
-    mlp = hk.nets.MLP([200,500,nx*ny],
+    mlp = hk.nets.MLP([200,nx*ny],
                         activation=jax.nn.tanh,
                         w_init=hk.initializers.VarianceScaling(1.0,"fan_avg","uniform"))
     return mlp(x)
@@ -64,13 +71,13 @@ def update(state: TrainingState, x, y):
 
 # ======================== train ==============================
 
-def fit(x_train,y_train,x_val,y_val,state,epochs):
+def fit(x_train,y_train,x_val,y_val,state,epochs,f_name):
     loss_train = []
     loss_val = []
 
     best_state = state
     min_loss = np.inf
-    for i in range(1,epochs):
+    for i in range(1,epochs+1):
         
         xx_train = jax.random.permutation(jax.random.PRNGKey(i),
                                             x_train,axis=0,
@@ -87,6 +94,7 @@ def fit(x_train,y_train,x_val,y_val,state,epochs):
         if l_val < min_loss:
             best_state = state
             min_loss = l_val
+            state_utils.save(Path("./local_results"),state,f_name)
 
         if i%200 == 0:
             print(f'epoch: {i}, loss: {l:.7f}')
@@ -94,8 +102,13 @@ def fit(x_train,y_train,x_val,y_val,state,epochs):
 
     return state, loss_train, loss_val
 
+pb_train = jnp.reshape(pb_train,(train_test_split[0],-1))
+pb_val = jnp.reshape(pb_val,(train_test_split[1],-1))
 
 # training ux
+print("Start training ux ...")
+ux_train = jnp.reshape(ux_train,(train_test_split[0],-1))
+ux_val = jnp.reshape(ux_val,(train_test_split[1],-1))
 rng = jax.random.PRNGKey(1)
 params = mdl.init(rng,pb_train[0,:]) # initalise weights
 opt_state = optimizer.init(params)
@@ -106,12 +119,15 @@ ux_state, ux_loss_train, ux_loss_val = fit(pb_train,
                                             pb_val,
                                             ux_val,
                                             ux_state,
-                                            epochs)
+                                            epochs,
+                                            "ux_state")
 
-state_utils.save(Path("./local_results"),ux_state,"ux_state")
 
 
 # training uy
+print("Start training uy ...")
+uy_train = jnp.reshape(uy_train,(train_test_split[0],-1))
+uy_val = jnp.reshape(uy_val,(train_test_split[1],-1))
 rng = jax.random.PRNGKey(2)
 params = mdl.init(rng,pb_train[0,:]) # initalise weights
 opt_state = optimizer.init(params)
@@ -122,9 +138,19 @@ uy_state, uy_loss_train, uy_loss_val = fit(pb_train,
                                             pb_val,
                                             uy_val,
                                             uy_state,
-                                            epochs)
+                                            epochs,
+                                            "uy_state")
 
-state_utils.save(Path("./local_results"),uy_state,"uy_state")
+
+# ================= save results =======================
+
+pb_train = pb_train.reshape((train_test_split[0],n_base))
+pb_val = pb_val.reshape((train_test_split[1],n_base))
+ux_train = ux_train.reshape((train_test_split[0],nx,ny))
+ux_val = ux_val.reshape((train_test_split[1],nx,ny))
+uy_train = uy_train.reshape((train_test_split[0],nx,ny))
+uy_val = uy_val.reshape((train_test_split[1],nx,ny))
+
 
 with h5py.File(Path("./local_results/results.h5"),'w') as hf:
     hf.create_dataset("ux_loss_train",data=np.array(ux_loss_train))
@@ -143,16 +169,16 @@ with h5py.File(Path("./local_results/results.h5"),'w') as hf:
     hf.create_dataset("pb_val", data=pb_val)
     hf.create_dataset("pb_test", data=pb_test)
 
-    hf.create_dataset("ux_train_m", data=ux_train_m)
-    hf.create_dataset("ux_val_m", data=ux_val_m)
-    hf.create_dataset("ux_test_m", data=ux_test_m)
+    hf.create_dataset("ux_train_m", data=xm_train[0,...])
+    hf.create_dataset("ux_val_m", data=xm_val[0,...])
+    hf.create_dataset("ux_test_m", data=xm_test[0,...])
 
-    hf.create_dataset("uy_train_m", data=uy_train_m)
-    hf.create_dataset("uy_val_m", data=uy_val_m)
-    hf.create_dataset("uy_test_m", data=uy_test_m)
+    hf.create_dataset("uy_train_m", data=xm_train[1,...])
+    hf.create_dataset("uy_val_m", data=xm_val[1,...])
+    hf.create_dataset("uy_test_m", data=xm_test[1,...])
 
-    hf.create_dataset("pb_train_m", data=pb_train_m)
-    hf.create_dataset("pb_val_m", data=pb_val_m)
-    hf.create_dataset("pb_test_m", data=pb_test_m)
+    hf.create_dataset("pb_train_m", data=xm_train[2,...])
+    hf.create_dataset("pb_val_m", data=xm_val[2,...])
+    hf.create_dataset("pb_test_m", data=xm_test[2,...])
 
 print("Finished at: ", time.asctime(time.localtime(time.time())))
