9d07a3b7a661422d4f1ebbc6be8ebe34b778c77b
y.mo <97536008+yaxinm@users.noreply.github.com>
2023-03-23 16:04:17 +0000

    Bug fix: stop abseil logging

--- utils/system.py
+++ utils/system.py
@@ -1,4 +1,16 @@
 from jax.lib import xla_bridge
+import sys
 
 def on_which_platform():
     print(xla_bridge.get_backend().platform)
+
+
+# https://github.com/abseil/abseil-py/issues/99
+# Work around TensorFlow's absl.logging depencency which alters the
+# default Python logging output behavior when present.
+def temporary_fix_absl_logging(level:str = 'warning'):
+    if 'absl.logging' in sys.modules:
+        import absl.logging
+        absl.logging.set_verbosity(level)
+        absl.logging.set_stderrthreshold(level)
+        # and any other apis you want, if you want
