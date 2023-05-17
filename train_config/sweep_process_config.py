a493eb72645e7587a0b603df7151ec918630c61f
y.mo <97536008+yaxinm@users.noreply.github.com>
2023-05-17 17:45:42 +0100

    Modified sweep pre-processing function

--- train_config/sweep_process_config.py
+++ train_config/sweep_process_config.py
@@ -4,13 +4,6 @@ from ast import literal_eval
 def sweep_preprocess_cfg(cfg):
     # example: cfg._layer1=100, cfg._layer2=200
     # should become cfg.layers=[100,200] 
-    if cfg._nb_cnn_layers==1:
-        cfg.update({'cnn_channels':(3,)}, allow_val_change=True)
-    elif cfg._nb_cnn_layers==2:
-        cfg.update({'cnn_channels':(16,3)}, allow_val_change=True)
-    elif cfg._nb_cnn_layers==3:
-        cfg.update({'cnn_channels':(32,16,3)}, allow_val_change=True)
-    
     t = literal_eval(cfg._cnn_filters)
     cfg.update({'cnn_filters':t}, allow_val_change=True)
     
