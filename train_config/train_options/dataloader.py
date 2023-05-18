c477259edfe5fa56fe97617f4d715e33fc6a8bc8
y.mo <97536008+yaxinm@users.noreply.github.com>
2023-05-18 16:23:37 +0100

    Use global variable with abseil to deal with noisy data

--- train_config/train_options/dataloader.py
+++ train_config/train_options/dataloader.py
@@ -3,7 +3,7 @@ from ml_collections.config_dict import ConfigDict
 
 from utils import simulation2d
 from utils.py_helper import slice_from_tuple
-from flowrec.data import data_partition, unnormalise_group, normalise
+from flowrec.data import data_partition, unnormalise_group, normalise,get_whitenoise_std
 
 import jax.numpy as jnp
 from jax.tree_util import Partial
@@ -14,6 +14,9 @@ import warnings
 import logging
 logger = logging.getLogger(f'fr.{__name__}')
 import chex
+
+from absl import flags
+FLAGS = flags.FLAGS
 
 
 def dataloader_example(data_config:ConfigDict):
@@ -50,7 +53,8 @@ def dataloader_2dtriangle(cfg:ConfigDict) -> dict:
         - u_val: same shape as u_train.\n
         - inn_train: pre-processed pressure measurement at the base of the triangle, has shape [t,len].\n
         - inn_val: same shape as inn_train.\n
-        If normalise, data dict also has train_minmax and val_minmax [3,range], range is [min,max]
+        - If normalise, data dict also has train_minmax and val_minmax [3,range], range is [min,max]\n
+        - If noisy, data dict also has u_train_clean and u_val_clean.
     
     datainfo: of class _Metadata2d.
     
@@ -82,10 +86,24 @@ def dataloader_2dtriangle(cfg:ConfigDict) -> dict:
 
     # Add white noise
     if cfg.snr:
+        logger.info('Saving clean data for calculating true loss')
+        [x_train,x_val,_], _ = data_partition(x,1,cfg.train_test_split,REMOVE_MEAN=cfg.remove_mean,randseed=randseed,SHUFFLE=cfg.shuffle) # Do not shuffle, do not remove mean for training with physics informed loss
+        [ux_train,uy_train,pp_train] = np.squeeze(np.split(x_train,3,axis=0))
+        [ux_val,uy_val,pp_val] = np.squeeze(np.split(x_val,3,axis=0))
+        u_train = np.stack((ux_train,uy_train,pp_train),axis=-1)
+        u_val = np.stack((ux_val,uy_val,pp_val),axis=-1)
+        data.update({
+            'u_train_clean': u_train,
+            'u_val_clean': u_val
+        })
+        
+
         logger.info('Adding white noise to data.')
+        FLAGS._noisy = True
         std_data = np.std(x,axis=(1,2,3),ddof=1)
-        snr_l = 10.**(cfg.snr/10.)
-        std_n = np.sqrt(std_data**2/snr_l)
+        # snr_l = 10.**(cfg.snr/10.)
+        # std_n = np.sqrt(std_data**2/snr_l)
+        std_n = get_whitenoise_std(cfg.snr,std_data)
         noise_ux = np.random.normal(scale=std_n[0],size=x[0,...].shape)
         noise_uy = np.random.normal(scale=std_n[1],size=x[1,...].shape)
         noise_pp = np.random.normal(scale=std_n[2],size=x[2,...].shape)
