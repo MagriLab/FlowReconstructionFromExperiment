0662badc4aed9c27998ece37ddd4815818deb302
y.mo <97536008+yaxinm@users.noreply.github.com>
2023-06-23 18:22:15 +0100

    use minimum loss to compare with previous runs in sweep

--- train.py
+++ train.py
@@ -421,7 +421,7 @@ def main(_):
         
         try: 
             logger.debug('Trying to save model as an artifact.')
-            if loss_val[-1] < best_run.summary['loss_val']:
+            if np.min(loss_val) < best_run.summary['loss_val']:
                 logger.info('Best model so far, saving weights and configurations.')
                 artifact = wandb.Artifact(name=f'sweep_weights_{run.sweep_id}', type='model') 
                 artifact.add_dir(tmp_dir)
