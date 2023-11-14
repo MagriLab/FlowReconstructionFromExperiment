26b93e8dc103149dc946a226786b2616d8ab84d7
y.mo <97536008+yaxinm@users.noreply.github.com>
2023-11-14 16:27:25 +0000

    make train compatiable with forcing term
    	non-standard data from 'data' dictionary is extracted to use as kwargs

--- train.py
+++ train.py
@@ -251,7 +251,7 @@ def fit(
     return state, loss_dict
 
 
-
+standard_data_keys = ['u_train_clean', 'u_val_clean', 'train_minmax', 'val_minmax', 'u_train', 'u_val', 'inn_train', 'inn_val', 'y_train', 'y_val']
 
 
 def main(_):
@@ -342,6 +342,7 @@ def main(_):
         'val_minmax':val_minmax 
     })
     logger.debug(f'Data dict now has {data.keys()}')
+    data_extra = {k: data[k] for k in data if k not in standard_data_keys}
 
     percent_observed = 100*(observed_train.size/data['u_train'].size)
     if run:
@@ -385,7 +386,8 @@ def main(_):
         cfg,
         datainfo = datainfo,
         take_observation = take_observation,
-        insert_observation = insert_observation
+        insert_observation = insert_observation,
+        **data_extra
     )
     logger.info('Created loss function.')
 
