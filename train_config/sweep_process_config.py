b1cee178520ca932278189e18d93b6ce8e425799
y.mo <97536008+yaxinm@users.noreply.github.com>
2024-03-22 18:25:48 +0000

    Update for sweeps

--- train_config/sweep_process_config.py
+++ train_config/sweep_process_config.py
@@ -10,10 +10,19 @@ def sweep_preprocess_cfg(cfg):
     # t2 = literal_eval(cfg._cnn_channels)
     # cfg.update({'cnn_channels':t2}, allow_val_change=True)
 
-    # print(cfg.slice_grid_sensors)
-    # step = int(cfg._slice_grid_sensors_step)
-    # cfg.update({'slice_grid_sensors':((None,None,step),(None,None,step))}, allow_val_change=True)
-    # print(cfg.slice_grid_sensors)
+    b1_channels = literal_eval(cfg._b1_channels)
+    scheduler = {
+        'constant': 'constant',
+        'exponential_decay': 'exponential_decay',
+        'cyclic_decay': "{'scheduler':'cyclic_cosine_decay_schedule','decay_steps':(800,1000,1200,1500),'alpha':(0.3,0.3,0.38,0.38),'lr_multiplier':(1.0,0.75,0.5,0.5),'boundaries':(1000,2200,3600,5500)}"
+    }
 
+    cfg.update(
+        {
+        'b1_channels': b1_channels,
+        'lr_scheduler': scheduler[cfg._lr_scheduler]
+        },
+        allow_val_change=True
+    )
     
     return cfg
